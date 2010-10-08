ee42b78aa1b6c1760dc6cfacc520009428b4936f
ticoerrecart <ticoerrecart@5a6bf194-4179-11de-9805-9778bc541ee5>
2010-10-08 03:40:28 +0000

    arreglo del getSessionId para los WS

---
+++ src/rytsa/itau/ws/ConversorWStoESB.java
@@ -0,0 +1,121 @@
+package rytsa.itau.ws;
+
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import javax.xml.ws.Holder;
+
+import rytsa.itau.utils.DateUtils;
+import rytsa.itau.valuaciones.dto.ndf.OperacionNDFAValuarData;
+import rytsa.itau.valuaciones.dto.ndf.RecuperoOperacionesNDFAValuarResponse;
+import client.ws.rytsa.seguridad.BEEstado;
+import client.ws.rytsa.seguridad.BESesiones;
+import client.ws.rytsa.seguridad.WSSeguridad;
+import client.ws.rytsa.seguridad.WSSeguridadSoap;
+import client.ws.rytsa.valuaciones.OperacionesNDFAValuarRequestData;
+import client.ws.rytsa.valuaciones.WSValuaciones;
+import client.ws.rytsa.valuaciones.WSValuacionesSoap;
+
+public class ConversorWStoESB {
+
+	private static String IP;
+	private static String USER_NAME;
+	private static String PASSWORD;
+	private static ResourceBundle resourceBundle;
+
+	static {
+		resourceBundle = ResourceBundle.getBundle("config");
+		IP = resourceBundle.getString("ws.ip");
+		USER_NAME = resourceBundle.getString("ws.userName");
+		PASSWORD = resourceBundle.getString("ws.password");
+	}
+
+	public static String getLogin() {
+		WSSeguridad wsSeguridad = new WSSeguridad();
+		WSSeguridadSoap proxySeguridad = wsSeguridad.getWSSeguridadSoap();
+		BESesiones bESesiones = new BESesiones();
+		proxySeguridad.loginSesion(IP, USER_NAME, PASSWORD, new Holder(bESesiones), new Holder(
+				new BEEstado()));
+		return bESesiones.getID();
+	}
+
+	public static void logout(String pIdSesion) {
+		WSSeguridad wsSeguridad = new WSSeguridad();
+		WSSeguridadSoap proxySeguridad = wsSeguridad.getWSSeguridadSoap();
+		proxySeguridad.logout(pIdSesion);
+	}
+
+	public static RecuperoOperacionesNDFAValuarResponse getOperacionesNDFAValuarWS(
+			Date pFechaProceso) throws Exception {
+		String idSession = getLogin();
+		WSValuaciones wsValu = new WSValuaciones();
+		WSValuacionesSoap proxy = wsValu.getWSValuacionesSoap();
+		OperacionesNDFAValuarRequestData request = new OperacionesNDFAValuarRequestData();
+		request.setFechaProceso(DateUtils.getXMLGregorianCalendar(pFechaProceso));
+		List<client.ws.rytsa.valuaciones.ArrayOfOperacionNDFAValuarData.OperacionNDFAValuarData> operaciones = proxy
+				.recuperarOperacionesNDFAValuar(idSession, request).getOperacionNDFAValuarData();
+		return getRecuperoOperacionesNDFAValuarResponse(operaciones);
+	}
+
+	private static RecuperoOperacionesNDFAValuarResponse getRecuperoOperacionesNDFAValuarResponse(
+			List<client.ws.rytsa.valuaciones.ArrayOfOperacionNDFAValuarData.OperacionNDFAValuarData> operaciones) {
+		RecuperoOperacionesNDFAValuarResponse r = new RecuperoOperacionesNDFAValuarResponse();
+		for (client.ws.rytsa.valuaciones.ArrayOfOperacionNDFAValuarData.OperacionNDFAValuarData operacionOrigen : operaciones) {
+			OperacionNDFAValuarData operacionDestino = new OperacionNDFAValuarData();
+			operacionDestino.setBanca(operacionOrigen.getBanca());
+			operacionDestino.setCantidadVN(operacionOrigen.getCantidadVN().doubleValue());
+			operacionDestino.setClinum(operacionOrigen.getCliNum());
+			operacionDestino.setComprasVigentes(operacionOrigen.getComprasVigentes().doubleValue());
+			operacionDestino.setContraParte(operacionOrigen.getContraParte());
+			operacionDestino.setCUIT(operacionOrigen.getCuit());
+			operacionDestino.setDev_Cob(operacionOrigen.getDevCob().doubleValue());
+			operacionDestino.setDev_Pag(operacionOrigen.getDevPag().doubleValue());
+			operacionDestino.setDev_total(operacionOrigen.getDevTotal().doubleValue());
+			operacionDestino.setDias(String.valueOf(operacionOrigen.getDias()));
+			operacionDestino.setFecha_proceso(operacionOrigen.getFechaProceso()
+					.toGregorianCalendar().getTime());
+			operacionDestino.setFechaCargaOperacion(operacionOrigen.getFechaCarga()
+					.toGregorianCalendar().getTime());
+			operacionDestino.setFechaInicio(operacionOrigen.getFechaInicio().toGregorianCalendar()
+					.getTime());
+			operacionDestino.setFechaVencimiento(operacionOrigen.getFechaVencimiento()
+					.toGregorianCalendar().getTime());
+			operacionDestino.setGestorRiesgoMercado(operacionOrigen.getGestorRiesgo());
+			operacionDestino.setIdOperacion(String.valueOf(operacionOrigen.getIDOperacion()));
+			// operacionDestino.setInstrumento(
+			operacionDestino.setMercado(operacionOrigen.getMercado());
+			operacionDestino.setMetodoFixing(operacionOrigen.getMetodoFixing());
+			operacionDestino.setMoneda(operacionOrigen.getMoneda());
+			operacionDestino.setMoneda_de_liquidacion(operacionOrigen.getMonedaLiquidacion());
+			operacionDestino.setMTM(operacionOrigen.getMTM().doubleValue());
+			operacionDestino.setNumeroBoleto(String.valueOf(operacionOrigen.getNumeroBoleto()));
+			operacionDestino.setNumeroOperacion(String
+					.valueOf(operacionOrigen.getNumeroOperacion()));
+			operacionDestino.setOfficer(operacionOrigen.getOfficer());
+			operacionDestino.setPlazo(String.valueOf(operacionOrigen.getPlazo()));
+			operacionDestino.setPrecio(operacionOrigen.getPrecio().doubleValue());
+			// operacionDestino.setPrecioCierreAnteriorContable(
+			// operacionDestino.setPrecioCierreContable(
+			// operacionDestino.setPrecioCierreContableMesAnterior(
+			operacionDestino.setProducto(operacionOrigen.getProducto());
+			operacionDestino.setRaiz(operacionOrigen.getRaiz());
+			operacionDestino.setRdo_BBA(operacionOrigen.getRdoBBA().doubleValue());
+			operacionDestino.setRdo_BBA_no_Dev(operacionOrigen.getRdoBBANoDev().doubleValue());
+			operacionDestino.setRdoDiarioContable(operacionOrigen.getRdoDiarioContab()
+					.doubleValue());
+			operacionDestino.setRdoMesContable(operacionOrigen.getRdoMesContab().doubleValue());
+			operacionDestino.setRdoTotalContable(operacionOrigen.getRdoTotalContab().doubleValue());
+			operacionDestino.setStrike_ref(operacionOrigen.getStrikeRef().doubleValue());
+			operacionDestino.setTipoOperacion(operacionOrigen.getTipoOperacion());
+			operacionDestino.setVencidos(operacionOrigen.getVencidos().doubleValue());
+			operacionDestino.setVentasVigentes(operacionOrigen.getVentasVigentes().doubleValue());
+			operacionDestino.setVigentes(operacionOrigen.getVigentes().doubleValue());
+
+			r.addOperacionNDFAValuarData(operacionDestino);
+		}
+
+		return r;
+	}
+
+}
